665a99429b299ed61323acad3bf21abe5d34e1ad
angelostori <angelostori13@gmail.com>
2026-02-13 18:04:52 +0100

    define the relation has-a betrween the two classes and check that works

--- index.php
+++ index.php
@@ -15,12 +15,14 @@ class Movie {
     private $title;
     private $director;
     private $year;
+    private $genre;
     private $description;
 
-    public function __construct($_title, $_director, $_year, $_description) {
+    public function __construct($_title, $_director, $_year, Genre $_genre, $_description) {
         $this->title = $_title;
         $this->director = $_director;
         $this->year = $_year;
+        $this->genre = $_genre;
         $this->description = $_description;
     }
 
@@ -58,14 +60,18 @@ class Movie {
 }
 
 $Avatar_description = "Un film di fantascienza che racconta la storia di un ex-marine che si ritrova su un pianeta alieno e si innamora di una donna nativa.";
-$Avatar = new Movie("Avatar", "James Cameron", 2009, $Avatar_description);
+$Avatar_genre = new Genre("Fantascienza", true);
+
+$Avatar = new Movie("Avatar", "James Cameron", 2009, $Avatar_genre, $Avatar_description);
 
 var_dump($Avatar);
 
 echo "----------------------------------<br/>";
 
 $Inception_description = "Un film di fantascienza che segue un ladro che entra nei sogni delle persone per rubare segreti, ma viene incaricato di impiantare un'idea nella mente di qualcuno.";
-$Inception = new Movie("Inception", "Christopher Nolan", 2010, $Inception_description);
+$Inception_genre = new Genre("Fantascienza", true);
+
+$Inception = new Movie("Inception", "Christopher Nolan", 2010, $Inception_genre, $Inception_description);
 
 var_dump($Inception);
 ?>
